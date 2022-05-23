43dd26e14da18d955cb84b278da1b780a3eadd63
ShinHeeSeung <tlsgmltmd726529@gmail.com>
2022-05-23 11:30:15 +0900

    양식 수정

--- xls_to_json.py
+++ xls_to_json.py
@@ -2,30 +2,33 @@ import openpyxl
 import json
 import pyexcel as p
 
-excel_file = 'D:\신희승\IIRTECH\data2\963510_50000.xls'
-json_file = 'D:\신희승\IIRTECH\data2\963510_50000.json'
+#excel_file = 'C:\Users\82105\Desktop\신희승\IIRTECH\IIRTECH\data.xlsx'
+json_file = 'C:\\Users\\82105\\Desktop\\신희승\\IIRTECH\\IIRTECH\\data.json'
+excel_file = 'C:\\Users\\82105\\Desktop\\신희승\\IIRTECH\\IIRTECH\\data.xlsx'
 
-p.save_book_as(file_name = excel_file, dest_file_name= excel_file+"x")
-# 엑셀 열기
+# xls to xlsx
+#p.save_book_as(file_name = excel_file, dest_file_name= excel_file+"x")
 
-excel_file = 'D:\신희승\IIRTECH\data\963510_50000.xlsx'
 wb = openpyxl.load_workbook(excel_file, read_only=True)
 
-sheet = wb.worksheets[0]
+sheet = wb.worksheets[1]
 
+# key_list 생성
+col_max = sheet.max_column
+row_max = sheet.max_row
 key_list = []
-for col_num in range(1, 7):
-    key_list.append(sheet.cell(row=1, column=col_num).value)
+for i in range(1,col_max+1):
+    key_list.append(sheet.cell(row=1, column=i).value)
 data_dict = {}
 key_index = 0
-for row_num in range(2, 100):
+for row_num in range(2, row_max):
     tmp_dict = {}
     if row_num % 10 == 0:
         print(row_num)
-    for col_num in range(1, 7):
-        val = sheet.cell(row = row_num, column= col_num).value
-        tmp_dict[key_list[col_num-1]] = val
-    data_dict[tmp_dict[key_list[key_index]]] = tmp_dict
+    for i in range(1, col_max+1):
+        val = sheet.cell(row=row_num, column=i).value
+        tmp_dict[key_list[i-1]] = val
+    data_dict[tmp_dict[key_list[0]] + tmp_dict[key_list[1]]] = tmp_dict
 wb.close()
 
 with open(json_file, 'w', encoding='utf-8') as fp:
