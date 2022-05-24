27dd9e056a27e7fbb589d7375b5dd2dbbc8c33d6
ShinHeeSeung <tlsgmltmd726529@gmail.com>
2022-05-24 11:27:14 +0900

    key값 변경

--- xls_to_json.py
+++ xls_to_json.py
@@ -1,5 +1,6 @@
 import openpyxl
 import json
+from collections import OrderedDict
 import pyexcel as p
 
 #excel_file = 'C:\Users\82105\Desktop\신희승\IIRTECH\IIRTECH\data.xlsx'
@@ -9,27 +10,28 @@ excel_file = 'C:\\Users\\82105\\Desktop\\신희승\\IIRTECH\\IIRTECH\\data.xlsx'
 # xls to xlsx
 #p.save_book_as(file_name = excel_file, dest_file_name= excel_file+"x")
 
-wb = openpyxl.load_workbook(excel_file, read_only=True)
+wb = openpyxl.load_workbook(excel_file)
 
 sheet = wb.worksheets[1]
 
 # key_list 생성
 col_max = sheet.max_column
 row_max = sheet.max_row
-key_list = []
+key_list = ['word', 'sense_no', 'definition', 'word_unit', 'word_type', 'original_language', 'language_type', 'pronunciation', 'conjugation', 'conjugation_pro', 'abbreviation', 'abbreviation_pro', 'origin', 'allomorph', 'pos',
+           'type', 'grammar', 'cat', 'example', 'source', 'origin', 'relation_word', 'translation', 'region']
 for i in range(1,col_max+1):
     key_list.append(sheet.cell(row=1, column=i).value)
-data_dict = {}
-key_index = 0
+
+data_list = []
 for row_num in range(2, row_max):
-    tmp_dict = {}
+    data = OrderedDict()
     if row_num % 10 == 0:
         print(row_num)
     for i in range(1, col_max+1):
-        val = sheet.cell(row=row_num, column=i).value
-        tmp_dict[key_list[i-1]] = val
-    data_dict[tmp_dict[key_list[0]] + tmp_dict[key_list[1]]] = tmp_dict
+        data[key_list[i-1]] = sheet.cell(row=row_num, column=i).value
+    data_list.append(data)
+
 wb.close()
 
 with open(json_file, 'w', encoding='utf-8') as fp:
-    json.dump(data_dict, fp, indent=4, ensure_ascii=False)
+    json.dump(data_list, fp, indent=4, ensure_ascii=False)
