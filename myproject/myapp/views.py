bb25d9e3bd6133d2190c0c2ad717c3f9239b1183
ShinHeeSeung <tlsgmltmd726529@gmail.com>
2022-05-26 12:03:41 +0900

    Django

---
+++ myproject/myapp/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+topics = [
+    {'id': 1, 'title': 'routing', 'body': ' Routing is ..'},
+    {'id': 2, 'title': 'view', 'body': 'view is ..'},
+    {'id': 3, 'title': 'model', 'body': 'model is ..'}
+]
+
+
+def HTMLTemplate(articleTag):
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</h1>
+        <ul>
+            {ol}
+        </ul>
+        {articleTag}
+        <ul>
+            <li><a href="/create/">create</a></li>
+        </ul>
+    </body>
+    </html>
+    '''
+
+
+# Create your views here.
+def index(request):
+    article = '''
+        <h2>Welcome</h2>
+        Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
+
+
+def read(request, id):
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
+
+
+@csrf_exempt
+def create(request):
+    article = '''
+    <form action="/create/">
+        <p><input type = "text" name="title" placeholder="title"></p> 
+        <p><textarea name = "body" placeholder="body"></textarea></p>
+        <p><input type ="submit"></p>
+    </form>
+    '''
+    return HttpResponse(HTMLTemplate(article))
