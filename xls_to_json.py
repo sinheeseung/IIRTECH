f78ce0a4653310dac117cb375ce3c5902601d0d8
ShinHeeSeung <tlsgmltmd726529@gmail.com>
2022-06-27 12:10:42 +0900

    target_code pk로 사용

--- xls_to_json.py
+++ xls_to_json.py
@@ -3,12 +3,12 @@ import json
 from collections import OrderedDict
 import pyexcel as p
 
-#excel_file = 'C:\Users\82105\Desktop\신희승\IIRTECH\IIRTECH\data.xlsx'
+# excel_file = 'C:\Users\82105\Desktop\신희승\IIRTECH\IIRTECH\data.xlsx'
 json_file = 'C:\\Users\\82105\\Desktop\\신희승\\IIRTECH\\IIRTECH\\data.json'
 excel_file = 'C:\\Users\\82105\\Desktop\\신희승\\IIRTECH\\IIRTECH\\data.xlsx'
 
 # xls to xlsx
-#p.save_book_as(file_name = excel_file, dest_file_name= excel_file+"x")
+# p.save_book_as(file_name = excel_file, dest_file_name= excel_file+"x")
 
 wb = openpyxl.load_workbook(excel_file)
 
@@ -17,9 +17,11 @@ sheet = wb.worksheets[1]
 # key_list 생성
 col_max = sheet.max_column
 row_max = sheet.max_row
-key_list = ['word', 'sense_no', 'definition', 'word_unit', 'word_type', 'original_language', 'language_type', 'pronunciation', 'conjugation', 'conjugation_pro', 'abbreviation', 'abbreviation_pro', 'origin', 'allomorph', 'pos',
-           'type', 'grammar', 'cat', 'example', 'source', 'origin', 'relation_word', 'translation', 'region']
-for i in range(1,col_max+1):
+key_list = ['word', 'sense_no', 'definition', 'word_unit', 'word_type', 'original_language', 'language_type',
+            'pronunciation', 'conjugation', 'conjugation_pro', 'abbreviation', 'abbreviation_pro', 'origin',
+            'allomorph', 'pos',
+            'type', 'grammar', 'cat', 'example', 'source', 'origin', 'relation_word', 'translation', 'region', 'pk']
+for i in range(1, col_max + 1):
     key_list.append(sheet.cell(row=1, column=i).value)
 
 data_list = []
@@ -27,8 +29,8 @@ for row_num in range(2, row_max):
     data = OrderedDict()
     if row_num % 10 == 0:
         print(row_num)
-    for i in range(1, col_max+1):
-        data[key_list[i-1]] = sheet.cell(row=row_num, column=i).value
+    for i in range(1, col_max + 1):
+        data[key_list[i - 1]] = sheet.cell(row=row_num, column=i).value
     data_list.append(data)
 
 wb.close()
