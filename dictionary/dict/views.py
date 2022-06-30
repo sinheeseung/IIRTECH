51bc65a3910c32fbaebc8ec34ce3dfdb3daa63e6
ShinHeeSeung <tlsgmltmd726529@gmail.com>
2022-06-30 14:05:54 +0900

    속담/관용구, 원어/언어 추가

--- dictionary/dict/views.py
+++ dictionary/dict/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 from elastic_app_search import Client
 
 '''
-원어/ 언어
+원어/ 언어 0
 활용/ 발음 0
 준말/ 발음 0
 용례  0
 관련 어휘/ 유형 0
 대역어/ 언어 0
-속담/뜻풀이/유형
+속담/뜻풀이/유형 0
 '''
 client = Client(
     # elastic app search engine의 Credentials
@@ -42,9 +42,9 @@ def main(request):
     return render(request, 'dict//base.html')
 
 
-def get_result(result, string):
+def get_result(result, string, separator):
     if result[string]['raw'] is not None:
-        return result[string]['raw'].split(', ')
+        return result[string]['raw'].split(separator)
     return
 
 
@@ -52,7 +52,7 @@ def get_pro_result(result, string):
     value = []
     if result[string]['raw'] is not None:
         value = result[string]['raw'].split('], ')
-    for i in range(len(value)-1):
+    for i in range(len(value) - 1):
         value[i] = value[i] + "]"
     return value
 
@@ -60,31 +60,44 @@ def get_pro_result(result, string):
 def index(request, pk):
     search_result = client.search(engine_name, pk, {})
     result = search_result['results'][0]
-    example = get_result(result, "example")
+    example = get_result(result, "example", ', ')
 
-    conjugation = get_result(result, "conjugation")
+    conjugation = get_result(result, "conjugation", ', ')
     conjugation_pro = get_pro_result(result, "conjugation_pro")
     conju_dic = {}
     if conjugation is not None and conjugation_pro is not None:
         conju_dic = {name: value for name, value in zip(conjugation, conjugation_pro)}
 
-    abbreviation = get_result(result, "abbreviation")
+    abbreviation = get_result(result, "abbreviation", ', ')
     abbreviation_pro = get_pro_result(result, "abbreviation_pro")
     abbre_dic = {}
     if abbreviation is not None and abbreviation_pro is not None:
         abbre_dic = {name: value for name, value in zip(abbreviation, abbreviation_pro)}
 
-    relation_word = get_result(result, "relation_word")
-    relation_type = get_result(result, "relation_type")
+    relation_word = get_result(result, "relation_word", ', ')
+    relation_type = get_result(result, "relation_type", ', ')
     relation_dic = {}
     word_list = []
     if relation_word is not None:
         for i in range(len(relation_word)):
-            if i != 0 and relation_type[i] != relation_type[i-1]:
-                relation_dic[relation_type[i-1]] = word_list
+            if i != 0 and relation_type[i] != relation_type[i - 1]:
+                relation_dic[relation_type[i - 1]] = word_list
                 word_list = []
             word_list.append(relation_word[i])
-        relation_dic[relation_type[len(relation_type)-1]] = word_list
+        relation_dic[relation_type[len(relation_type) - 1]] = word_list
+    proverb = get_result(result, 'proverb', '   ')
+    pro_defi = get_result(result, 'pro_defi', '   ')
+    pro_type = get_result(result, 'pro_type', '   ')
+    proverb_dict = {}
+    if proverb is not None:
+        for i in range(len(proverb)):
+            proverb_dict[proverb[i]] = {pro_defi[i]: pro_type[i]}
 
+    original_language = get_result(result, 'original_language', ', ')
+    language_type = get_result(result, 'language_type', ', ')
+    language_dict = {}
+    if original_language is not None:
+        language_dict = {name: value for name, value in zip(original_language, language_type)}
     return render(request, 'dict//item.html', {'result': result, 'example': example,
-                                               'conjugation': conju_dic, 'abbreviation': abbre_dic, 'relation': relation_dic})
+                                               'conjugation': conju_dic, 'abbreviation': abbre_dic,
+                                               'relation': relation_dic, 'proverb': proverb_dict, 'language': language_dict})
